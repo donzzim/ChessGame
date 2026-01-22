e836ac3319f3dd8cb428296755a07a5f20211d12
Lucas César <lucascesarmengo@gmail.com>
2026-01-22 17:32:28 -0300

    ChessPosition's create;

--- Program.cs
+++ Program.cs
@@ -2,18 +2,6 @@
 using ChessGame.chessboard;
 using ChessGame.chess;
 
-try
-{
-    Chessboard cb = new Chessboard(8, 8);
 
-    cb.PlacePiece(new Rook(cb, Color.Black), new Position(0, 0));
-    cb.PlacePiece(new Rook(cb, Color.Black), new Position(1, 3));
-    cb.PlacePiece(new King(cb, Color.Black), new Position(0, 9));
-    Screen.printScreen(cb);
-}
-catch (ChessboardException e)
-{
-    Console.WriteLine(e.Message);
-}
-
-Console.ReadLine();
+ChessPosition cp = new ChessPosition(1, 'a');
+Console.WriteLine(cp.ToPosition());
---
+++ chess/ChessPosition.cs
@@ -0,0 +1,25 @@
+﻿using ChessGame.chessboard;
+
+namespace ChessGame.chess;
+
+public class ChessPosition
+{
+    public char Column { get; set; }
+    public int Row { get; set; }
+
+    public ChessPosition(int row,char column)
+    {
+        Column = column;
+        Row = row;
+    }
+
+    public Position ToPosition()
+    {
+        return new Position(8 - Row, Column - 'a');
+    }
+
+    public override string ToString()
+    {
+        return "" + Column + Row;
+    }
+}
