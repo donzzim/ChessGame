9a70801bf7a613a2252546100baacb7da14da6e5
Lucas César <lucascesarmengo@gmail.com>
2026-01-22 17:40:15 -0300

    Improving implementation methods

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 using ChessGame.chessboard;
 using ChessGame.chess;
 
+try
+{
+    Chessboard cb = new Chessboard(8, 8);
 
-ChessPosition cp = new ChessPosition(1, 'a');
-Console.WriteLine(cp.ToPosition());
+    cb.PlacePiece(new Rook(cb, Color.Black), new Position(0, 0));
+    cb.PlacePiece(new Rook(cb, Color.Black), new Position(1, 3));
+    cb.PlacePiece(new King(cb, Color.Black), new Position(0, 9));
+    Screen.PrintScreen(cb);
+}
+catch (ChessboardException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
--- Screen.cs
+++ Screen.cs
@@ -1,12 +1,15 @@
 ﻿
+using ChessGame.chessboard;
+
 namespace ChessGame;
 
 public class Screen
 {
-    public static void printScreen(chessboard.Chessboard cb)
+    public static void PrintScreen(chessboard.Chessboard cb)
     {
         for (int i = 0; i < cb.Rows; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < cb.Columns; j++)
             {
                 if (cb.Piece(i, j) == null)
@@ -15,11 +18,28 @@ public class Screen
                 }
                 else
                 {
-                    Console.Write(cb.Piece(i, j) + " ");
+                    PrintPiece(cb.Piece(i, j));
+                    Console.Write(" ");
                 }
             }
-
             Console.WriteLine();
+        }
+
+        Console.WriteLine(" a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if (piece.Color == Color.White)
+        {
+            Console.WriteLine(piece);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
         }
     }
 }
